6a53533b71ee0ea378100aec1ab2de3e9b27dc36
WooLegend <dnwogus4260@naver.com>
2024-09-24 04:18:23 +0900

    [Rafactor] 리스트 페이지 게시물 카드 삽입 로직 변경
    - 기존 insetAdjacentHTML을 사용해 문자열 형태의 요소를 삽입하는 방식에서
      요소를 생성한 후 append하는 방식으로 변경
    - 태그명을 전체에서 All로 변경

--- public/js/list.js
+++ public/js/list.js
@@ -5,7 +5,7 @@ const tags = [
   { tag: "algorithm", icon: "fa-solid fa-code", title: "Algorithm" },
   { tag: "htmlcss", icon: "fa-brands fa-html5", title: "HTML/CSS" },
   { tag: "javascript", icon: "fa-brands fa-js", title: "Javascript" },
-  { tag: "nodejs", icon: "fa-brands fa-node-js", title: "Node.js" },
+  { tag: "nodejs", icon: "fa-brands fa-node-js", title: "NodeJS" },
   { tag: "react", icon: "fa-brands fa-react", title: "React" },
   { tag: "other", icon: "fa-solid fa-ellipsis", title: "Other" }
 ]
@@ -20,7 +20,7 @@ tags.forEach(e => {
 
   const a = document.createElement('a');
   a.setAttribute('href', location.pathname + `?tag=${e.tag}`);
-  tag.classList.add('tag-link');
+  a.classList.add('tag-link');
   a.textContent = e.title;
 
   const i = document.createElement('i');
@@ -35,18 +35,28 @@ tags.forEach(e => {
 
 function addPostCards(posts) {
   posts.forEach(e => {
-    const thumbnail = e.thumbnail || "/asset/thumbnail-placeholder-500x334.webp"
-    const card = `
-          <div class="card" data-id="${e._id}">
-            <img src="${thumbnail}" alt="thumbnail" width="100%">
-            <div class="card-content">
-              <h4 style="overflow:hidden; display:inline-block;width:max-content;">${e.title}</h4>
-              <p>${e.username}</p>
-              <p style="opacity: 0.5; font-weight: bold; font-size: 0.7rem;">${e.date}</p>
-            </div>
-          </div>
-          `
-    cardContainer.insertAdjacentHTML('beforeend', card)
+    const thumbnail = null || "/asset/thumbnail-placeholder-500x334.webp"
+    
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.id = e._id;
+    const img = document.createElement('img');
+    img.src = thumbnail;
+    img.classList.add('card-thumbnail');
+    const main = document.createElement('div');
+    main.classList.add('card-main');
+    const title = document.createElement('h4');
+    title.textContent = e.title;
+    const author = document.createElement('p');
+    author.textContent = e.username;
+    const date = document.createElement('p');
+    date.classList.add('card-date')
+    date.textContent = e.date;
+
+    main.append(title, author, date);
+    card.append(img, main);
+    
+    cardContainer.appendChild(card)
 
     const newCard = cardContainer.lastElementChild
     newCard.addEventListener('click', () => {
